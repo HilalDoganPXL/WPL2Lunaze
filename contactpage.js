37d9697e275a4aff74ef936f433d6c81bfab15b5
hilaldoganPXL <hilal.dogan@student.pxl.be>
2021-05-31 04:12:54 +0200

    contact page changed css en html

--- contactpage.js
+++ contactpage.js
@@ -1,29 +1,36 @@
-function validateForm() {
-    var name =  document.getElementById('name').value;
+function validateForm(name,email,message) {
+
     if (name == "") {
+        alert("please provide your name!")
         document.querySelector('.status').innerHTML = "Name cannot be empty";
+        name.focus();
         return false;
     }
-    var email =  document.getElementById('email').value;
-    if (email == "") {
+
+    if (email.empty()) {
+        alert("please provide your email!")
         document.querySelector('.status').innerHTML = "Email cannot be empty";
+        email.focus();
         return false;
     } else {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if(!re.test(email)){
             document.querySelector('.status').innerHTML = "Email format invalid";
+            email.focus();
             return false;
         }
     }
-    var subject =  document.getElementById('subject').value;
-    if (subject == "") {
-        document.querySelector('.status').innerHTML = "Subject cannot be empty";
-        return false;
-    }
-    var message =  document.getElementById('message').value;
-    if (message == "") {
+
+    if (message.empty()) {
         document.querySelector('.status').innerHTML = "Message cannot be empty";
+        message.focus();
         return false;
     }
     document.querySelector('.status').innerHTML = "Sending...";
 }
+let name =  document.getElementById('name').value;
+let email =  document.getElementById('email').value;
+let message =  document.getElementById('message').value;
+
+document.getElementById('contact-form').addEventListener('submit', validateForm(name,email,message));
+

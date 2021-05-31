2a20c07950b1bcc2c1d81d78f701af3670017f83
hilaldoganPXL <hilal.dogan@student.pxl.be>
2021-05-31 20:06:08 +0200

    Shop page html and css

---
+++ lunaze.js
@@ -0,0 +1,7 @@
+let gekozenProduct = document.getElementById("gekozenproduct");
+
+document.getElementById("gekozenproduct").innerHTML = "hallo";
+
+let product = document.getElementById("product_form");
+
+console.log("djfhfljsfdhf"+product);

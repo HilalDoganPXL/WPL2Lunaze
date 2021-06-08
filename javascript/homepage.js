71f31793cb72e9bade29a370aae581e9118f5cc8
hilaldoganPXL <hilal.dogan@student.pxl.be>
2021-06-08 02:44:57 +0200

    go to top button javascript

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,42 +1,16 @@
 'use strict'
 
-// Function to add event listener to table
-//let up = document.getElementById("up");
-/*let uppage = document.getElementById('up');
 
-uppage.onclick = () => window.scrollTo({ top: 0, behavior: 'smooth' })
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-*/
-
-//document.addEventListener("click", topFunction);
-//up.document.addEventListener("click", topFunction);
-
-let go_up = document.getElementById("btn_up");
-
-window.onscroll = function () {
-    scrollFunction();
-};
-
-function scrollFunction() {
-    if (
-        document.body.scrollTop > 20 ||
-        document.documentElement.scrollTop > 20
-    ) {
-        go_up.style.display = "block";
-    } else {
-        go_up.style.display = "none";
-    }
-}
-function backToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+function handleLoad() {
+    document.getElementById("btn_up").addEventListener("click", go_up);
+   alert("test");
 }
 
-go_up.addEventListener("click", backToTop);
+window.addEventListener("load", handleLoad);
 
+
+function go_up(){
+    window.scrollTo(0, 1)
+}
 
 
